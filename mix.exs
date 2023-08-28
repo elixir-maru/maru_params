6879b7aa771b955292dc7b297f3dc7396bb775c1
falood <falood@gmail.com>
2023-08-28 13:49:48 +0900

    bump v0.2.10

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MaruParams.MixProject do
   def project do
     [
       app: :maru_params,
-      version: "0.2.9",
+      version: "0.2.10",
       elixir: "~> 1.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
